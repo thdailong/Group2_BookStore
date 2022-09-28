9af26da47f2d6a2f5ac0e022e5a5af23ea61c6a9
MAC <mac@MacBook-Pro-cua-MAC.local>
2022-09-28 09:18:46 +0700

    Update project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Group2_BookStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
